665c3056678f95b081cb41572b92117cfdd0fac1
Sabick-A <sabicksabi@gmail.com>
2024-11-09 01:16:34 +0530

    csv parsing and data preview

--- src/pages/Data/UploadData.jsx
+++ src/pages/Data/UploadData.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import Papa from "papaparse";
+import { parseCsv } from "../../utils/FormData/parseCsv.js";
 
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import SelectOne from "../../components/FormElements/SelectOne";
@@ -11,29 +11,10 @@ function UploadData() {
     const { register, handleSubmit, watch } = useForm();
     const [error, setError] = useState("");
 
+    const [parsedData, setParsedData] = useState(null);
+
     const closeModal = () => {
         setError(null);
-    };
-
-    const parseCsv = (file) => {
-        return new Promise((resolve, reject) => {
-            try {
-                console.log("parsing started");
-                Papa.parse(file, {
-                    header: true,
-                    complete: function (results) {
-                        resolve(results);
-                    },
-                    error: (error) => {
-                        console.error("UploadData.jsx :: parseCsv :: Papa.parse :: error", error);
-                        reject(error);
-                    },
-                });
-            } catch (error) {
-                console.error("UploadData.jsx :: parseCsv :: tryCatch :: error", error);
-                reject(error);
-            }
-        });
     };
 
     const handleData = async (data) => {
@@ -41,75 +22,190 @@ function UploadData() {
         if (selectedOption === "CSV File Upload") {
             const file = watch("csvFile")[0];
             if (file) {
-                const parsedData = await parseCsv(file);
+                const response = await parseCsv(file);
+                response.name = data.name;
+                setParsedData(response);
                 console.log(parsedData);
-            }
-            else{
+            } else {
                 setError("Please upload a CSV file");
             }
         }
+        console.log(parsedData);
     };
     return (
         <>
             <Breadcrumb pageName="Upload Data" />
             {error && <ErrorModal {...{ error, closeModal }} />}
-            <form onSubmit={handleSubmit(handleData)}>
-                <div className=" bg-slate-50 w-11/12">
-                    <div className="flex flex-col rounded-sm border w-full border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark p-5 gap-5">
-                        <div>
-                            <label className="mb-3 block text-black dark:text-white">
-                                Give A Name for the Data
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="Data Name"
-                                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                                {...register("name", {
-                                    required: true,
-                                })}
-                            />
-                        </div>
-                        <SelectOne
-                            label={"Select Upload Source"}
-                            options={["CSV File Upload", "Google Sheets Url"]}
-                            setSelectedOption={setSelectedOption}
-                            selectedOption={selectedOption}
-                        />
-                        {selectedOption === "CSV File Upload" && (
+            <div className="grid grid-cols-1 gap-9 sm:grid-cols-1">
+                <form onSubmit={handleSubmit(handleData)}>
+                    <div className=" bg-slate-50 w-11/12">
+                        <div className="flex flex-col rounded-sm border w-full border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark p-5 gap-5">
                             <div>
                                 <label className="mb-3 block text-black dark:text-white">
-                                    Attach CSV file
-                                </label>
-                                <input
-                                    type="file"
-                                    className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
-                                    {...register("csvFile")}
-                                />
-                            </div>
-                        )}
-
-                        {selectedOption === "Google Sheets Url" && (
-                            <div>
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Enter Google Sheets URL
+                                    Give A Name for the Data
                                 </label>
                                 <input
                                     type="text"
-                                    placeholder="Google Sheet Url"
+                                    placeholder="Data Name"
                                     className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                                    {...register("gsheetUrl")}
+                                    {...register("name", {
+                                        required: true,
+                                    })}
                                 />
                             </div>
-                        )}
-                        <button
-                            type="submit"
-                            className="inline-flex items-center justify-center rounded-md bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-                        >
-                            Fetch Data
-                        </button>
+                            <SelectOne
+                                label={"Select Upload Source"}
+                                options={[
+                                    "CSV File Upload",
+                                    "Google Sheets Url",
+                                ]}
+                                setSelectedOption={setSelectedOption}
+                                selectedOption={selectedOption}
+                            />
+                            {selectedOption === "CSV File Upload" && (
+                                <div>
+                                    <label className="mb-3 block text-black dark:text-white">
+                                        Attach CSV file
+                                    </label>
+                                    <input
+                                        type="file"
+                                        className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                                        {...register("csvFile")}
+                                    />
+                                </div>
+                            )}
+
+                            {selectedOption === "Google Sheets Url" && (
+                                <div>
+                                    <label className="mb-3 block text-black dark:text-white">
+                                        Enter Google Sheets URL
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Google Sheet Url"
+                                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                                        {...register("gsheetUrl")}
+                                    />
+                                </div>
+                            )}
+                            <button
+                                type="submit"
+                                className="inline-flex items-center justify-center rounded-md bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+                            >
+                                Fetch Data
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+
+                {parsedData && (
+                    <>
+                        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+                            Data Preview
+                        </h2>
+                        <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+                            <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+                                {parsedData.name}
+                            </h4>
+                            <div className="flex flex-col overflow-x-auto">
+                                <div className="min-w-full grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-6">
+                                    <div className="p-2.5 xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Name
+                                        </h5>
+                                    </div>
+                                    <div className="p-2.5 xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Company Name
+                                        </h5>
+                                    </div>
+                                    <div className="p-2.5 text-center xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Email
+                                        </h5>
+                                    </div>
+                                    <div className="hidden p-2.5 text-center sm:block xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Location
+                                        </h5>
+                                    </div>
+                                    <div className="hidden p-2.5 text-center sm:block xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Products
+                                        </h5>
+                                    </div>
+                                    <div className="hidden p-2.5 text-center sm:block xl:p-5">
+                                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                                            Others
+                                        </h5>
+                                    </div>
+                                </div>
+
+                                {parsedData.data.map((dataRow, index) => (
+                                    <div
+                                        className={`min-w-full grid grid-cols-3 sm:grid-cols-6 ${
+                                            index === parsedData.data.length - 1
+                                                ? ""
+                                                : "border-b border-stroke dark:border-strokedark"
+                                        }`}
+                                        key={index}
+                                    >
+                                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.name || "N/A"}
+                                            </p>
+                                        </div>
+                                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.companyName || "N/A"}
+                                            </p>
+                                        </div>
+                                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.email || "N/A"}
+                                            </p>
+                                        </div>
+                                        <div className="hidden sm:flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.location || "N/A"}
+                                            </p>
+                                        </div>
+                                        <div className="hidden sm:flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.products || "N/A"}
+                                            </p>
+                                        </div>
+                                        <div className="hidden sm:flex items-center justify-center p-2.5 xl:p-5">
+                                            <p className="text-black dark:text-white">
+                                                {dataRow.others.length > 0
+                                                    ? dataRow.others.join(", ")
+                                                    : "N/A"}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            {parsedData.errors.length > 0 && (
+                                <div className="my-4 text-red-500">
+                                    <h5 className="text-lg font-medium">
+                                        Errors Occured While Parsing:
+                                    </h5>
+                                    <ul className="px-10">
+                                        {parsedData.errors.map(
+                                            (error, index) => (
+                                                <li key={index}>
+                                                    {error.message} (Row:{" "}
+                                                    {error.row})
+                                                </li>
+                                            )
+                                        )}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
         </>
     );
 }
---
+++ src/utils/FormData/parseCsv.js
@@ -0,0 +1,91 @@
+import Papa from "papaparse";
+
+function parseData(inputData) {
+    // Initialize result object
+    const result = {
+      data: [],
+      errors: inputData.errors || []
+    };
+  
+    // Extract the field names from the meta section
+    const fields = inputData.meta.fields || [];
+  
+    // Define standard column names for matching
+    const standardColumns = {
+      name: ['name', 'Name', 'Full Name'],
+      companyName: ['Company name', 'Organization name','company name','Company Name','company Name','Company','company'],
+      email: ['email','Email','Email Address','email address'],
+      location: ['location','Location','Location of the company','location of the company','Location of the Company','location of the Company'],
+      products: ['products','Products','Products Offered','Products of the company','products of the company','Products of the Company','products of the Company']
+    };
+  
+    // Iterate over each data item
+    inputData.data.forEach(row => {
+      const parsedRow = {
+        name : '',
+        companyName: '',
+        email: '',
+        location: '',
+        products: '',
+        others: []
+      };
+  
+      // Check for mismatched or missing fields and process the row
+      fields.forEach((fieldName) => {
+        const value = row[fieldName] || '';
+        
+        // Check if the field matches any of the standard column names
+        let matched = false;
+        for (let [key, names] of Object.entries(standardColumns)) {
+          if (names.includes(fieldName)) {
+            parsedRow[key] = value;
+            matched = true;
+            break;
+          }
+        }
+  
+        // If the field doesn't match, convert it to "Column Name: Value" format and add to others
+        if (!matched && value) {
+            parsedRow.others.push(JSON.stringify({ [fieldName]: value }));
+          }
+      });
+      
+      // Set companyName as name if name is missing
+      if (!parsedRow.name) {
+        parsedRow.name = parsedRow.companyName;
+    }
+    
+      // Check if all fields are empty
+      const isEmptyRow = !parsedRow.name && !parsedRow.companyName && !parsedRow.email && !parsedRow.location && !parsedRow.products && parsedRow.others.length === 0;
+
+      // Push the parsed row into the result data array only if it's not empty
+      if (!isEmptyRow) {
+          result.data.push(parsedRow);
+      }
+    });
+    console.log("parsed data", result);
+    return result;
+  }
+  
+const parseCsv = (file) => {
+    return new Promise((resolve, reject) => {
+        try {
+            console.log("parsing started");
+            Papa.parse(file, {
+                header: true,
+                complete: function (results) {
+                    resolve(parseData(results));
+                },
+                error: (error) => {
+                    console.error("UploadData.jsx :: parseCsv :: Papa.parse :: error", error);
+                    reject(error);
+                },
+            });
+        } catch (error) {
+            console.error("UploadData.jsx :: parseCsv :: tryCatch :: error", error);
+            reject(error);
+        }
+    });
+};
+
+export { parseCsv };
